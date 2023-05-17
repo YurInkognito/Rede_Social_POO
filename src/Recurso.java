d4a93e961543a9badcc46a35c0077c79ac704693
Yuri <71910210+YurInkognito@users.noreply.github.com>
2023-05-17 12:00:46 -0300

    Add: adding first codes

---
+++ src/Recurso.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Recurso {
+
+    int ID;
+    String url_recurso;
+
+    int prox_ID;
+
+    String str = "validaUrlRecurso";
+    boolean bool = Boolean.parseBoolean(str);
+
+
+
+}
