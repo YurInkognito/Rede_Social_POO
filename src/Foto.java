6a47b1287c99369ac907986b306869763b831249
Yuri <71910210+YurInkognito@users.noreply.github.com>
2023-06-22 16:52:57 -0300

    Add: main file, add if statement for que the resolucao of Foto (with erros)

--- src/Foto.java
+++ src/Foto.java
@@ -2,4 +2,10 @@ package src;
 
 public class Foto {
     String resolucao;
+
+    if (resolucao.endsWith(".jpg, .png, .bmp")){
+        return Boolean.valueOf(String);
+    }else {
+        System.out.println("Este arquivo não está em um formato suportável\n");
+    }
 }
